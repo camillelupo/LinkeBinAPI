ec0e41bf32ea754884d0d4397f08773a9921f158
17359 <camille.lupo@hotmail.fr>
2020-01-07 14:40:48 +0100

    ajout controllers avec les fichiers

---
+++ src/Controller/BinController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bin;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BinController extends AbstractController
+{
+    /**
+     * @Route("/bin", name="bin")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BinController.php',
+        ]);
+    }
+
+
+    public function addBin(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+
+
+    }
+}
---
+++ src/Controller/CityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\City;
+
+class CityController extends AbstractController
+{
+    /**
+     * @Route("/city", name="city")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CityController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/addCity", name="addCity")
+     */
+    public function createCity(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $city = new City();
+        $city->setName('test');
+
+        $raw = file_get_contents('C:\Users\17359\PhpstormProjects\LinkeBinAPI\toulouse.json');
+        $json = json_decode($raw,true);
+        $city->setFileJson($json);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($city);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new city with id '.$city->getId());
+    }
+}
