5acfc62e84d0dd54c9651e1465f4b4f7e8577f83
17359 <camille.lupo@hotmail.fr>
2020-01-09 16:23:06 +0100

    tests bincontroller

--- src/Controller/BinController.php
+++ src/Controller/BinController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Bin;
 use App\Entity\City;
+use App\Entity\CityBin;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,19 +29,41 @@ class BinController extends AbstractController
     public function addAllBins($id): Response
     {
 
+
+        /*$entityManager = $this->getDoctrine()->getManager();*/
         $city = $this->getDoctrine()
             ->getRepository(City::class)
             ->find($id);
-
-
         $json = $city->getFileJson();
 
+        $bin = new bin();
 
-        var_dump($json);
+        //Requete permettant d'ajouter les bin en fonction du json de la ville
+
+        //ajout dans la table jointure
+        /*$cityBin = new cityBin();
+        $cityBin->setUuidCity($id);*/
+
+      /*  // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($cityBin);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();*/
+
+
+        $lat =$json['features'][0]['properties']['geo_point_2d'][0];
+        $lon =$json['features'][0]['properties']['geo_point_2d'][1];
+        echo "<pre>";
+        var_dump($lat);
+        var_dump($lon);
+        echo "</pre>";
+
+
+
+
         $entityManager = $this->getDoctrine()->getManager();
 
-        return new Response(var_dump($json));
-
+        return new Response();
 
     }
 }
