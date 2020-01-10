779cd3aa75e3e456b7aa8e76d21d6c78fb2ebccb
LambdamanDEV <Leroyque@hotmail.com>
2020-01-10 09:56:49 +0100

    Swagger marche et c'est bien, version 1

---
+++ swagger/swagger.php
@@ -0,0 +1,10 @@
+<?php
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(title="API baise", version="1.0")
+ * @OA\Server(
+ *     url="http://api.tutoriel.fr/v1",
+ *     description="Mon api"
+ * )
+ */
